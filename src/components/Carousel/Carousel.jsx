af9d8d01b49edde2c333088128310545d8f0df82
shubham-tech01 <shubham.pandey@techolution.com>
2024-05-08 10:24:45 +0530

    marvel code

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import CarouselCards from "../CarouselCard/CarouselCards";
 import axios from "axios";
 import styles from "./Carousel.module.css";
